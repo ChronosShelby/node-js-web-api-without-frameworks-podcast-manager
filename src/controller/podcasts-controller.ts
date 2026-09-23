13d81f79bbdedbbc4f472e86f49083b6f82af950
ChronosShelby <igorgabrielantunes252002@gmail.com>
2026-09-22 22:00:54 -0300

    Add: Project

---
+++ src/controller/podcasts-controller.ts
@@ -0,0 +1,30 @@
+import * as http from "http";
+import { serviceListEpisodes } from "../services/list-episodes-service";
+import { serviceFilterEpisodes } from "../services/filter-episodes-services";
+import { ContentType } from "../utils/content-type";
+import { PodcastTransferModel } from "../models/Podcast-Transfer-Model";
+
+
+const defaultContent= { "Content-Type": ContentType.JSON };
+
+export const getListEpisodes = async (
+    req: http.IncomingMessage, 
+    res: http.ServerResponse
+) => {
+    const content: PodcastTransferModel = await serviceListEpisodes();
+
+    res.writeHead(content.statusCode, defaultContent);
+    res.write(JSON.stringify(content.body));
+    res.end();
+}
+
+export const getFilterEpisodes = async (
+    req: http.IncomingMessage, 
+    res: http.ServerResponse
+) => {
+    const content: PodcastTransferModel = await serviceFilterEpisodes(req.url);
+
+    res.writeHead(content.statusCode, defaultContent);
+    res.write(JSON.stringify(content.body));
+    res.end();
+};
